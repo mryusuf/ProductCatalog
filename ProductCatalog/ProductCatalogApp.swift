28e81b95bb0bced4009489dc3ceba72e702125c7
mryusuf <123148968+i-yusuf-mufin@users.noreply.github.com>
2023-07-04 20:44:03 +0700

    Initial Commit

---
+++ ProductCatalog/ProductCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCatalogApp.swift
+//  ProductCatalog
+//
+//  Created by Indra Permana on 04/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ProductCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
